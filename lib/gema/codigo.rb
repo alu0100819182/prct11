1eb47c77ab944a8a7f1b6113be12ac2b9b589439
alu0100819182 <alu0100819182@ull.edu.es>
2015-11-05 19:37:26 +0000

    Solucionado la septima prueba

--- lib/gema/codigo.rb
+++ lib/gema/codigo.rb
@@ -1,11 +1,12 @@
 class Bibliografia
-    attr_reader :x, :y, :z, :w, :t, :s
-    def initialize(autor, titulo, editorial, edicion, publicacion, serie='0')
+    attr_reader :x, :y, :z, :w, :t, :s, :m
+    def initialize(autor, titulo, editorial, edicion, publicacion, isbn, serie='0')
         @x = autor
         @y = titulo
         @w = editorial
         @t = edicion
         @s = publicacion
+        @m = isbn
         @z = serie
     end
 end
