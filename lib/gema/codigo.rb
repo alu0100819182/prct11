d519f1331abd9fbcf274a94888a8724c7b7d1ba2
alu0100819182 <alu0100819182@ull.edu.es>
2015-11-05 19:21:59 +0000

    Solucionada la cuarta prueba

--- lib/gema/codigo.rb
+++ lib/gema/codigo.rb
@@ -1,8 +1,9 @@
 class Bibliografia
-    attr_reader :x, :y, :z
-    def initialize(autor, titulo, serie='0')
+    attr_reader :x, :y, :z, :w
+    def initialize(autor, titulo, editorial, serie='0')
         @x = autor
         @y = titulo
+        @w = editorial
         @z = serie
     end
 end
