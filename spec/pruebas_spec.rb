6ee638a3e8990b614a305ab0c3c4e544ae15b252
alu0100819182 <alu0100819182@ull.edu.es>
2015-11-05 19:30:44 +0000

    Fallo en la sexta prueba

--- spec/pruebas_spec.rb
+++ spec/pruebas_spec.rb
@@ -3,8 +3,8 @@ require 'spec_helper'
 
 describe Bibliografia do
     before :each do
-        @b1 = Bibliografia.new(['Dave Thomas', 'Andy Hunt', 'Chad Fowler'],'Programming Ruby 1.9 & 2.0: The Pragmatic Programmers’ Guide','Pragmatic Bookshelf',4,'The Facets of Ruby')
-        @b2 = Bibliografia.new(['Dave Thomas', 'Andy Hunt', 'Chad Fowler'],'Programming Ruby 1.9 & 2.0: The Pragmatic Programmers’ Guide','Pragmatic Bookshelf',4)
+        @b1 = Bibliografia.new(['Dave Thomas', 'Andy Hunt', 'Chad Fowler'],'Programming Ruby 1.9 & 2.0: The Pragmatic Programmers’ Guide','Pragmatic Bookshelf',4,'July 7, 2013','The Facets of Ruby')
+        @b2 = Bibliografia.new(['Dave Thomas', 'Andy Hunt', 'Chad Fowler'],'Programming Ruby 1.9 & 2.0: The Pragmatic Programmers’ Guide','Pragmatic Bookshelf',4,'July 7, 2013')
     end
     
     describe "Datos de la bibliografia" do
@@ -24,9 +24,13 @@ describe Bibliografia do
             @b1.w.should eq('Pragmatic Bookshelf')
             @b2.w.should eq('Pragmatic Bookshelf')
         end
-        it "Edicion" do
+        it "Numero de Edicion" do
             @b1.t.should eq(4)
             @b2.t.should eq(4)
+        end
+        it "Fecha de publicacion" do
+            @b1.s.should eq('July 7, 2013')
+            @b2.s.should eq('July 7, 2013')
         end
     end
 end
