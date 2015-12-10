ca4683d4a2ea09b3aeebe9b646df94216b6c5b2b
alu0100819182 <alu0100819182@ull.edu.es>
2015-12-10 15:05:16 +0000

    Añadida un par de expecificaciones

--- lib/gema/apa.rb
+++ lib/gema/apa.rb
@@ -2,6 +2,7 @@ require 'gema/codigo'
 require 'gema/listaenlazada'
 
 class APA
+    include Comparable
     attr_reader :o, :nombre
     def initialize (biblio)
         @o = biblio
@@ -10,9 +11,25 @@ class APA
     def nomape
         tam = @o.x.length
         for i in 0..tam-1
-            mensaje = "#{mensaje}"+"Autor, #{@o.ap[i][0]}. #{@o.x[i][0]}. "
+            if(i!=tam-1)
+                mensaje = "#{mensaje}"+"Autor, #{@o.ap[i][0]}. #{@o.x[i][0]}. & "
+            else
+                mensaje = "#{mensaje}"+"Autor, #{@o.ap[i][0]}. #{@o.x[i][0]}. "
+            end
         end
         mensaje
+    end
+    
+    def <=> (other)
+        if(@o.ap == other.ap)
+            if(@o.s == other.s)
+               @o.y <=> other.y
+            else
+                @o.s <=> other.s
+            end
+        else
+            @o.ap <=> ohter.ap
+        end
     end
     
 end
